0509f43b72e198769f9882f5890406c70f4c8dbd
Abduarraheem <elfandi.a23@gmail.com>
2020-10-03 15:02:40 -0700

    Got street names based on latitude and longitude and added a small test file.

--- locationParse.py
+++ locationParse.py
@@ -1,14 +1,18 @@
+'''
+Abduarraheem Elfandi and Juno Mayer.
+
+'''
 import os
 import utm
 import requests
 
 import gpxpy
 import gpxpy.gpx
-
-import anm_api_test
+import json
 
 # TODO make sure the GPX file is taken from the user not a predefined file
-gpx_file = open('Prototype/testGPX.gpx', 'r')
+print(os.getcwd())
+gpx_file = open('Project 1/Reverse-Geocode/TestFiles/smallGPX.gpx', 'r')
 gpx = gpxpy.parse(gpx_file)
 
 
@@ -20,10 +24,11 @@ class MyTrack:
     def __init__(self, trackName):
         self.trackName = trackName
 
+
 '''
     A location is a part of a track which contains a latitude,
-    longitude, elevation and the time that location has been reached.
-    TODO get the street names which will also be a part of the location 
+    longitude, elevation, the time that location has been reached
+    and the street name of where that location is at.
 '''
 class Location:
     def __init__(self, latitude: float, longitude: float, elevation: float, time: str, stName: str):
@@ -35,14 +40,14 @@ class Location:
     
 
 def getStName(lat: float, lng: float ):
-
-
-
+    '''
+    Function that gets the street name given the latitude and longitude.
+    '''
     payload  = {"apiKey" : "58bd566df1854af4885c680dd2c42dc1", "version" : "4.10", 
                 "lat" : lat, "lon" : lng, "format" : "JSON"}
 
     r = requests.get("http://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest/", params=payload)
-    print(r.text)
+    # print(r.text)
 
     result = json.loads(r.text)
 
@@ -55,13 +60,14 @@ for track in gpx.tracks:
     trackList.append(myTrack)
     for segment in track.segments:
         for point in segment.points:
-            myTrack.listLocation.append(Location(point.latitude, point.longitude, point.elevation, point.time, getStName(point.latitude, point,longitude)))
+            myTrack.listLocation.append(Location(point.latitude, point.longitude, point.elevation, point.time, getStName(point.latitude, point.longitude)))
 
 
-for i in range(len(trackList)):
-    listLoc = trackList[i].listLocation
-    print(f'Track Number: {i}')
-    for j in range(len(listLoc)):
-        print('Entry {0}: latitude {1}, longitude {2}, elevation {3}, time {4}, street {5}'
-        .format(j, listLoc[j].latitude, listLoc[j].longitude, listLoc[j].elevation, listLoc[j].time, listLoc[j].stName))
-    print(f'Track Name: {trackList[i].trackName} Track Number: {i}')
+# for i in range(len(trackList)):
+#     listLoc = trackList[i].listLocation
+#     print(f'Track Number: {i}')
+#     for j in range(len(listLoc)):
+#         print(f'Entry {j}: latitude {listLoc[j].latitude}, longitude {listLoc[j].longitude},\ 
+#         elevation {listLoc[j].elevation}, time {listLoc[j].time}, street {listLoc[j].stName}')
+
+#     print(f'Track Name: {trackList[i].trackName} Track Number: {i}')
