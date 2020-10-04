bd24aafa02d79b6dd720a0b37af46a5bb6e1a017
Abduarraheem <elfandi.a23@gmail.com>
2020-10-03 19:08:20 -0700

    Added error 403 and 404 handling to app.py, also fixed typo in 404.html file.

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template("base.html")
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template("404.html"), 404
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template("403.html"), 403
+
+
 if __name__ == "__main__":
     app.run(debug=True)
