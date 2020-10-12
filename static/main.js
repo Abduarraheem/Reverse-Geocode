1fa174923b4e458ef1737ba51d4070a8ca379221
Abduarraheem <elfandi.a23@gmail.com>
2020-10-11 20:58:48 -0700

    Dark mode mostly finished buttons and output remains.

--- static/main.js
+++ static/main.js
@@ -1,6 +1,34 @@
-$(document).ready(function(){
-    $('.nav-menu').on('click', function(){
-        $(this).toggleClass('open');
-        // $('.side-nav').toggleClass('open');
-    });
-});
+// variable which checks if the switch has been checked 
+const pressSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+function switchBtn(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-mode', 'dark'); // set the attibute to dark
+        localStorage.setItem('mode', 'dark'); // set the mode to document storage so we can use it if the user refreshes
+    }
+    else {
+        document.documentElement.setAttribute('data-mode', 'light');  // set the attibute to light
+        localStorage.setItem('mode', 'light'); // set the mode to document storage so we can use it if the user refreshes
+    }    
+}
+
+/* 
+listener that waits and checks for the switch to be pressed
+if pressed then the mode will be switched.
+*/
+pressSwitch.addEventListener('change', switchBtn, false); 
+
+// get the current mode from localStorage
+const currentMode = localStorage.getItem('mode') ? localStorage.getItem('mode') : null;
+
+/* 
+Gets the current mode from localStorage and sets that mode and if it is dark mode
+it make it so that the switch is checked.
+*/
+
+if (currentMode) {
+    document.documentElement.setAttribute('data-mode', currentMode);
+    if (currentMode === 'dark') {
+        pressSwitch.checked = true;
+    }
+}
