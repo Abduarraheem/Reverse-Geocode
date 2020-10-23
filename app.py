9c26542666403df3cb8865122bd635494deafac9
Juno <gjvmayer@gmail.com>
2020-10-23 14:33:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def index():
 @app.route('/give_coords')
 def give_coords(): 
     #also give tehe mapbox key 
-    if 'coords_list' in session:
+    if sesstion['coords_list'] != []:
         coords_list = session['coords_list']
         print(coords_list)
         zoom_value = 7
